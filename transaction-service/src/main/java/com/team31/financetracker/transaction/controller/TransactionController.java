6af904365cefb9f2af25ea9627028697bdedd1d7
Ahmed <ahmeddragon700@gmail.com>
2026-04-10 23:18:25 +0200

    feat(transaction-service) implement search (55-18839)

--- transaction-service/src/main/java/com/team31/financetracker/transaction/controller/TransactionController.java
+++ transaction-service/src/main/java/com/team31/financetracker/transaction/controller/TransactionController.java
@@ -1,7 +1,9 @@
 package com.team31.financetracker.transaction.controller;
 
+import com.team31.financetracker.transaction.Enums.TransactionStatus;
 import com.team31.financetracker.transaction.model.Transaction;
 import com.team31.financetracker.transaction.service.TransactionService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -34,6 +38,14 @@ public class TransactionController {
     @GetMapping
     public List<Transaction> getAllTransactions() {
         return transactionService.getAllTransactions();
+    }
+
+    @GetMapping("/search")
+    public List<Transaction> searchTransactions(
+            @RequestParam(required = false) TransactionStatus status,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+        return transactionService.searchByDateRangeAndOptionalStatus(startDate, endDate, status);
     }
 
     @GetMapping("/{id}")

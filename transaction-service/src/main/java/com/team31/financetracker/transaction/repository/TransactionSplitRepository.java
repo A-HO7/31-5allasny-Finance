41643983d0b6053541ccc6e4bb122392b1aba989
AhmedV100 <akotb6003@gmail.com>
2026-04-26 03:31:51 +0300

    Attempt to fix Transaction

--- transaction-service/src/main/java/com/team31/financetracker/transaction/repository/TransactionSplitRepository.java
+++ transaction-service/src/main/java/com/team31/financetracker/transaction/repository/TransactionSplitRepository.java
@@ -1,10 +1,14 @@
 package com.team31.financetracker.transaction.repository;
 
+import com.team31.financetracker.transaction.model.TransactionSplit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.team31.financetracker.transaction.model.TransactionSplit;
+import java.util.List;
 
 @Repository
 public interface TransactionSplitRepository extends JpaRepository<TransactionSplit, Long> {
+
+    /** Find all splits belonging to a given transaction, ordered by splitOrder. */
+    List<TransactionSplit> findByTransactionIdOrderBySplitOrderAsc(Long transactionId);
 }

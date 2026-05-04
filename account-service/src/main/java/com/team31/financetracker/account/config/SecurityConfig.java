75e93ebdfc59a7db148211739b114cb28ec1e03d
Youssef Sameh <youssef_sameh55@live.com>
2026-05-04 22:06:12 +0300

    feat (account): implement JWT Authentication on All Endpoints (55-10390)

--- account-service/src/main/java/com/team31/financetracker/account/config/SecurityConfig.java
+++ account-service/src/main/java/com/team31/financetracker/account/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.team31.financetracker.account.config;
 import com.team31.financetracker.account.config.auth.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,6 +13,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
@@ -32,7 +34,7 @@ public class SecurityConfig {
 
                 // 3. Public vs protected endpoints
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/accounts/health").permitAll()
+                        .requestMatchers("/api/accounts/health","api/statements/health").permitAll()
                         .anyRequest().authenticated()
                 )
 

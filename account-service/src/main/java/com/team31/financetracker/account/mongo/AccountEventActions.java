dcbd5599a53260f7934a2b80ad34d958126b3519
AhmedWaleedElFar <ahmedw.delfar2002@gmail.com>
2026-05-05 03:41:39 +0300

    fix(account): Add AccountEventActions as an Enum, and remove the duplicate import in AccountSearchDocument (55-1174)

--- account-service/src/main/java/com/team31/financetracker/account/mongo/AccountEventActions.java
+++ account-service/src/main/java/com/team31/financetracker/account/mongo/AccountEventActions.java
@@ -1,22 +1,22 @@
 package com.team31.financetracker.account.mongo;
 
-public class AccountEventActions {
+public enum AccountEventActions {
     // M2 new features
-    public static final String INDEXED = "INDEXED";
-    public static final String DASHBOARD_VIEWED = "DASHBOARD_VIEWED";
+    INDEXED,
+    DASHBOARD_VIEWED,
 
     // M1 retrofits
-    public static final String DETAILS_UPDATED = "DETAILS_UPDATED";
-    public static final String FROZEN = "FROZEN";
-    public static final String UNFROZEN = "UNFROZEN";
-    public static final String RATED = "RATED";
-    public static final String STATEMENT_VERIFIED = "STATEMENT_VERIFIED";
+    DETAILS_UPDATED,
+    FROZEN,
+    UNFROZEN,
+    RATED,
+    STATEMENT_VERIFIED,
 
     // CRUD
-    public static final String ACCOUNT_CREATED = "ACCOUNT_CREATED";
-    public static final String ACCOUNT_UPDATED = "ACCOUNT_UPDATED";
-    public static final String ACCOUNT_DELETED = "ACCOUNT_DELETED";
-    public static final String STATEMENT_CREATED = "STATEMENT_CREATED";
-    public static final String STATEMENT_UPDATED = "STATEMENT_UPDATED";
-    public static final String STATEMENT_DELETED = "STATEMENT_DELETED";
+    ACCOUNT_CREATED,
+    ACCOUNT_UPDATED,
+    ACCOUNT_DELETED,
+    STATEMENT_CREATED,
+    STATEMENT_UPDATED,
+    STATEMENT_DELETED
 }

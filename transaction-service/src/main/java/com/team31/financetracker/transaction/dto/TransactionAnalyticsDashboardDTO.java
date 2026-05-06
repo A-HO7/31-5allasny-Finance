0bb234e7a5c6d393de34cf7cd4e011c618dcf37f
AhmedV100 <akotb6003@gmail.com>
2026-05-06 19:15:48 +0300

    fix(transaction): apply builder pattern and jwt filter to transaction features (55-12283)

--- transaction-service/src/main/java/com/team31/financetracker/transaction/dto/TransactionAnalyticsDashboardDTO.java
+++ transaction-service/src/main/java/com/team31/financetracker/transaction/dto/TransactionAnalyticsDashboardDTO.java
@@ -10,4 +10,64 @@ public record TransactionAnalyticsDashboardDTO(
         Double completionRate,
         Map<String, Integer> transactionsByCategory,
         Map<String, Integer> transactionsByStatus) {
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private Integer totalTransactions;
+        private Double totalIncome;
+        private Double totalExpenses;
+        private Double netFlow;
+        private Double completionRate;
+        private Map<String, Integer> transactionsByCategory;
+        private Map<String, Integer> transactionsByStatus;
+
+        public Builder totalTransactions(Integer totalTransactions) {
+            this.totalTransactions = totalTransactions;
+            return this;
+        }
+
+        public Builder totalIncome(Double totalIncome) {
+            this.totalIncome = totalIncome;
+            return this;
+        }
+
+        public Builder totalExpenses(Double totalExpenses) {
+            this.totalExpenses = totalExpenses;
+            return this;
+        }
+
+        public Builder netFlow(Double netFlow) {
+            this.netFlow = netFlow;
+            return this;
+        }
+
+        public Builder completionRate(Double completionRate) {
+            this.completionRate = completionRate;
+            return this;
+        }
+
+        public Builder transactionsByCategory(Map<String, Integer> transactionsByCategory) {
+            this.transactionsByCategory = transactionsByCategory;
+            return this;
+        }
+
+        public Builder transactionsByStatus(Map<String, Integer> transactionsByStatus) {
+            this.transactionsByStatus = transactionsByStatus;
+            return this;
+        }
+
+        public TransactionAnalyticsDashboardDTO build() {
+            return new TransactionAnalyticsDashboardDTO(
+                    totalTransactions,
+                    totalIncome,
+                    totalExpenses,
+                    netFlow,
+                    completionRate,
+                    transactionsByCategory,
+                    transactionsByStatus);
+        }
+    }
 }

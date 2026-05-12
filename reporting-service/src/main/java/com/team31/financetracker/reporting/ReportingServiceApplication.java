62fa04ddaf1305df27982fc83c7e1e704bf83834
AhmedWaleedElFar <ahmedw.delfar2002@gmail.com>
2026-05-12 23:50:08 +0300

    add(inter-service-setup): OpenFeign Dependency (55-1174)

--- reporting-service/src/main/java/com/team31/financetracker/reporting/ReportingServiceApplication.java
+++ reporting-service/src/main/java/com/team31/financetracker/reporting/ReportingServiceApplication.java
@@ -2,8 +2,10 @@ package com.team31.financetracker.reporting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ReportingServiceApplication {
 
     public static void main(String[] args) {
